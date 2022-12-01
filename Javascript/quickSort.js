f6e16ceb80d9ed2298d1d7dad8f96b4c43ab3f8e
Sami Safatli <samisafatli@live.com>
2022-12-01 14:02:14 -0300

    Add quick sort algorithm

---
+++ Javascript/quickSort.js
@@ -0,0 +1,16 @@
+const quickSort = (arr) => {
+    if (arr.length < 2) return arr
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) left.push(arr[i]) 
+        else right.push(arr[i])
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+const arr = [8,20,-2,4,-6]
+console.log(quickSort(arr))
+// WorstCase - O(n2)
+// Avg case - O(nLogn)
