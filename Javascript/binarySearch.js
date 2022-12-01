942c083fe930f5dfe6321f09c1d4e0f43d03b046
Sami Safatli <samisafatli@live.com>
2022-12-01 12:54:12 -0300

    Add recursiveBinarySearch algorithm

--- Javascript/binarySearch.js
+++ Javascript/binarySearch.js
@@ -11,6 +11,25 @@ const binarySearch = (arr, target) => {
     return -1
 }
 
+const recursiveBinSearch = (arr, target) => {
+    return search(arr, target, 0, arr.length - 1)
+}
+
+const search = (arr, target, leftIndex, rightIndex) => {
+    if(leftIndex > rightIndex) return -1
+
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+    
+    if (target === arr[middleIndex]) return middleIndex
+
+    if (target < arr[middleIndex]) return search(arr, target, leftIndex, middleIndex - 1)
+    else return search(arr, target, middleIndex + 1, rightIndex)
+}
+
 console.log(binarySearch([-5,2,4,6,10], 10))
 console.log(binarySearch([-5,2,4,6,10], 6))
 console.log(binarySearch([-5,2,4,6,10], 20))
+
+console.log(recursiveBinSearch([-5,2,4,6,10], 10))
+console.log(recursiveBinSearch([-5,2,4,6,10], 6))
+console.log(recursiveBinSearch([-5,2,4,6,10], 20))
